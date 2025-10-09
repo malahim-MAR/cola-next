953113e747340a1c806c31733b65588198d0f3a4
malahim-mar <armalahim01@gmail.com>
2025-10-10 01:28:00 +0500

    navbar in testing

--- src/Pages/Testing.jsx
+++ src/Pages/Testing.jsx
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import "./Testing.css";
+import { FaSearch } from "react-icons/fa";
 
 const Testing = () => {
     const [open, setOpen] = useState(false);
 
     return (
         <>
-            <section className="navbar-full">  
-                  <nav className="navbar">
-                {/* Left Logo */}
-                <div className="logo">
-                    <img src={logo} alt="Cola Next Logo" />
-                </div>
+            <section className="navbar-full">
+                <nav className="navbar">
+                    {/* Left Logo */}
+                    <div className="logo">
+                        <img src={logo} alt="Cola Next Logo" />
+                    </div>
 
-                {/* Search Bar */}
-                <div className="search-box">
-                    <input type="text" placeholder="Search" />
-                </div>
+                    {/* Search Bar */}
+                    <div className="search-box">
+                        <i><FaSearch/></i>
+                        <input type="text" placeholder="Search" />
+                    </div>
 
-                {/* Hamburger Icon */}
-                <div
-                    className={`hamburger ${open ? "open" : ""}`}
-                    onClick={() => setOpen(!open)}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </nav>
+                    {/* Hamburger Icon */}
+                    <div
+                        className={`hamburger ${open ? "open" : ""}`}
+                        onClick={() => setOpen(!open)}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </nav>
 
             </section>
 
